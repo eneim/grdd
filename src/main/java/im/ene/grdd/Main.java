65c6f845211fd95982e6614fdc499ccfb9df491b
eneim <mr.nguyenhoainam@gmail.com>
2016-12-21 14:05:09 +0900

    Add temporary dependencies to confirm shadowJar, run shadowJar, update wercker

---
+++ src/main/java/im/ene/grdd/Main.java
@@ -0,0 +1,14 @@
+package im.ene.grdd;
+
+/**
+ * @author eneim
+ * @since 12/21/16
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Calc calc = new Calc();
+    double addResult = calc.add(1.0, 2.9);
+    System.out.print(addResult);
+  }
+}
