93af22c2a9cab37ea2ddb82883d15391dfdd54ce
eneim <mr.nguyenhoainam@gmail.com>
2017-06-19 15:50:16 +0900

    minor change

--- src/main/java/im/ene/grdd/Main.java
+++ src/main/java/im/ene/grdd/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
   public static void main(String[] args) {
     Calc calc = new Calc();
-    double addResult = calc.add(1.0, 2.9);
+    double addResult = calc.add(2.0, 2.9);
     System.out.print(addResult);
   }
 }
